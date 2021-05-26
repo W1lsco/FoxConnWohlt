32de94651a6b59946b9b95e42e5e349f42afc725
willwohlt <wlwohlt@uwm.edu>
2021-05-26 16:06:02 -0400

    set up config classes for OAuth

---
+++ src/main/java/com/word/apiDemo/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.word.apiDemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+
+
+@Configuration
+@EnableWebSecurity
+private class WebSecurityConfig {
+
+    
+}
