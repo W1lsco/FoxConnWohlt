0dee52bc5ff6d6dde387eed18c2562417a4ca59d
willwohlt <wlwohlt@uwm.edu>
2021-05-26 15:45:04 -0400

    got the controller set up, now on to the H2

---
+++ src/main/java/com/word/apiDemo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.word.apiDemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping(value = "/test", produces = "application/json")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
